bd2b2aef3688139f2012c411125338c32cf91c55
lulucas <ghost@void.com>
2020-04-27 09:36:30 +0800

    add refresh_token action

---
+++ account/token.go
@@ -0,0 +1,27 @@
+package account
+
+import (
+	"context"
+	pie "github.com/lulucas/hasura-pie"
+)
+
+func refreshToken(cc pie.CreatedContext) interface{} {
+	type RefreshTokenOutput struct {
+		Token string
+	}
+	return func(ctx context.Context) (*RefreshTokenOutput, error) {
+		session := cc.GetSession(ctx)
+		userId := session.UserId
+		if userId == nil {
+			return nil, ErrInvalidCredentials
+		}
+		// refresh token
+		token, err := pie.AuthJwt(userId.String(), session.Role)
+		if err != nil {
+			return nil, err
+		}
+		return &RefreshTokenOutput{
+			Token: token,
+		}, nil
+	}
+}
